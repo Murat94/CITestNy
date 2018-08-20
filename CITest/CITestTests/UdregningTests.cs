2e1d5a27f4a11c16cd0276195efdb29de53bf74c
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-08-20 10:00:18 +0200

    test projekt oprettet

---
+++ CITest/CITestTests/UdregningTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CITest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITest.Tests
+{
+    [TestClass()]
+    public class UdregningTests
+    {
+        [TestMethod()]
+        public void minusTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
