d25fd090f2fd904aeed1c5156c3ae0251aa2b931
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-08-20 10:23:57 +0200

    Unittest oprettet

---
+++ ConsoleAppTESTCI/ConsoleAppTESTCITests/UdregningTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppTESTCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTESTCI.Tests
+{
+    [TestClass()]
+    public class UdregningTests
+    {
+        [TestMethod()]
+        public void addTest()
+        {
+            Udregning test1 = new Udregning();
+
+         int acctual =   test1.add(4, 2);
+
+            Assert.AreEqual(6, acctual);
+        }
+
+        [TestMethod()]
+        public void addTestminus()
+        {
+            Udregning test2 = new Udregning();
+
+            int acctual = test2.minus(4, 2);
+
+            Assert.AreEqual(2, acctual);
+        }
+    }
+}
